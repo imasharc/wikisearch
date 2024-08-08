601b02b1816b03b36e860de9cfb39cd23199a150
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-08-08 11:15:50 +0200

    feat(results): Add search results display with embedded Wikipedia articles
    - Create esults.html template to display search results.
    - Implement dynamic display of search results using Flask's Jinja templating.
    - Add a link to go back to the search page.
    - Include links to toggle and embed Wikipedia articles within the results page.
    - Add script.js for handling the display and toggling of embedded Wikipedia articles.
    - Create styles.css for styling the results and embedded articles.
    
    This commit introduces the functionality to display search results and embed Wikipedia articles directly within the results page. Users can click on article titles to toggle the display of the article content and click on links to view the full articles on Wikipedia.

---
+++ static/script.js
@@ -0,0 +1,21 @@
+﻿function toggleArticle(url, index) {
+  // Get the selected article container and iframe
+  var container = document.getElementById("article-container-" + index);
+  var iframe = document.getElementById("article-frame-" + index);
+
+  // Check if the container is currently displayed
+  if (container.style.display === "block") {
+    // Hide the container if it is already displayed
+    container.style.display = "none";
+  } else {
+    // Hide all article containers
+    document
+      .querySelectorAll(".article-container")
+      .forEach(function (container) {
+        container.style.display = "none";
+      });
+    // Show the selected article container and set the iframe src
+    container.style.display = "block";
+    iframe.src = url;
+  }
+}
